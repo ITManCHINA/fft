ea921fcb14d3a182e8e15b4abc823cb7094f4a61
apoint123 <108002475+apoint123@users.noreply.github.com>
2026-05-12 23:03:40 +0800

    chore(deps): 移除已被弃用的 wee_alloc

---
+++ .changeset/dark-llamas-sit.md
@@ -0,0 +1,5 @@
+---
+"@applemusic-like-lyrics/fft": patch
+---
+
+chore(deps): 移除已被弃用的 wee_alloc
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,6 @@ mod resampler;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen(start)]
 fn start() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
